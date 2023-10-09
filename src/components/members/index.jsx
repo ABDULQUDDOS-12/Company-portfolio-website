7e070197f55e2d2a8ea5139e632d781c310f9896
kashanButt <kashanbutt566@gmail.com>
2023-10-10 04:42:23 +0500

    added data for members

--- src/components/members/index.jsx
+++ src/components/members/index.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import styles from "./members.module.css";
 import { FaXTwitter, FaDiscord } from "react-icons/fa6";
+import members from "./members.json";
 
 const Members = () => {
   return (
     <section className={styles.members}>
       <h1>Our Top Agents</h1>
       <div className={styles.agents} id="agents">
-        <div className="item">
-          <img src="" alt="" className={styles.avatar} />
-          <h3 className={styles.name}>Agent Name</h3>
-          <div className={styles.socials}>
-            <span>
-              <FaDiscord />
-            </span>
-            <span>
-              <FaXTwitter />
-            </span>
+        {members.map((item) => (
+          <div className={styles.item}>
+            <img src={item.avatar} alt="" className={styles.avatar} />
+            <h4 className={styles.name}>{item.name}</h4>
+            <div className={styles.socials}>
+              <span>
+                <a href={item.socials.discord} target="__blank">
+                  <FaDiscord />
+                </a>
+              </span>
+              <span>
+                <a href={item.socials.twitter} target="__blank">
+                  <FaXTwitter />
+                </a>
+              </span>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </section>
   );
